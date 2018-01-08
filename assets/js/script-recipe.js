13595ee11d84d34c39ed380299ab99a394d12c00
tamybl <tamara.bravo.l@gmail.com>
2018-01-08 20:53:23 -0300

    Etapa 8: evento click para clase make

--- assets/js/script-recipe.js
+++ assets/js/script-recipe.js
@@ -1,6 +1,10 @@
 $(document).ready( function () {
   $('.js-menu').hide();
-
-
+  $('.js-show-recipe').click( function () {
+    $(this).addClass("make");
+  })
+  $('.js-show-make').click( function () {
+    $('.js-show-recipe').removeClass("make");
+  })
 
 })
