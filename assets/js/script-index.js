312d491381e8290bfaf7065fd4dd1e93df80cda6
tamybl <tamara.bravo.l@gmail.com>
2018-01-08 10:12:19 -0300

    Etapa 1: Mostrar y ocultar iconos navbar

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -2,6 +2,7 @@ $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
+  $('.js-back').hide();
 
 });
 
