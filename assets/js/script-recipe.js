13b638a836480c837c06d15112d9cfe7fc8bfd4a
tamybl <tamara.bravo.l@gmail.com>
2018-01-09 00:25:33 -0300

    Etapa 9: clase active asignada a botones del menu

--- assets/js/script-recipe.js
+++ assets/js/script-recipe.js
@@ -2,9 +2,16 @@ $(document).ready( function () {
   $('.js-menu').hide();
   $('.js-show-make').click( function () {
     $('body').find('.page').addClass("make");
+    $(this).addClass("active");
+    $('.js-show-recipe').removeClass("active");
+
   })
+
   $('.js-show-recipe').click( function () {
     $('body').find('.page').removeClass("make");
+    $(this).addClass("active");
+    $('.js-show-make').removeClass("active");
+
   })
 
 })
